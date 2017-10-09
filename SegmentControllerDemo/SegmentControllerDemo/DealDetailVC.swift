c2cf1e7a5417347cb668da50270594c786370f89
BrijeshShiroya <brijesh281994@gmail.com>
2017-10-09 17:14:56 +0530

    add DealDetailVC

---
+++ SegmentControllerDemo/SegmentControllerDemo/DealDetailVC.swift
@@ -0,0 +1,33 @@
+//
+//  DealDetailVC.swift
+//  SegmentControllerDemo
+//
+//  Created by Creole02 on 10/9/17.
+//  Copyright © 2017 CreoleStudios. All rights reserved.
+//
+
+import UIKit
+
+class DealDetailVC: UIViewController {
+
+  //MARK: - Outlets
+  
+  //MARK: - Variables
+  
+  //MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+  //MARK: - Other functions
+  
+
+  //MARK: - Actions
+}
